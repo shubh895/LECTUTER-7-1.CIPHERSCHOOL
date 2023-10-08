44feb996db24e1bb86bda039d44a710dbfe7d8e9
Shubham Pandey <119473042+shubh895@users.noreply.github.com>
2023-10-08 17:05:35 +0530

    Add files via upload

---
+++ lecture_7.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int passward;
+    // cout<<"enter the passward: ";
+    // cin>>passward;
+    // while(passward<999999)
+    // {
+    //     cout<<"The passward does not meet the requirement , enter the passward again : ";
+    //     cin>>passward;
+    // }
+    // cout<<"the passward is correct";
+    // do{
+    //     cin>>passward;
+    // }
+    // while(passward<999999)
+
+    // int count;
+    // while(1)    // infinite while loop
+    // {
+    //     cout<<"chocolate";
+    //     count++;
+    //     if(count>100){
+    //         break;
+    //     }
+    // }
+     // infinite for loop
+    int i;
+    for(i=0; ;i++){
+        cout<<i;
+        if(i>100){break;}
+    }
+
+
+
+}
